600f7ca79582bf78ba1090739151ac8fe5df5c10
robincwillis <robincwillis@gmail.com>
2015-05-17 13:22:26 -0400

    don't persist collections

--- example/example.js
+++ example/example.js
@@ -3,8 +3,14 @@ Colors = new Mongo.Collection('colors');
 
 
 if (Meteor.isClient) {
-	// counter starts at 0
-	Session.setDefault('counter', 0);
+
+	Template.hello.created = function(){
+		Meteor.call('reset', function(err, res){
+			if(err){
+				console.log(err);
+			}
+		});
+	}
 
 	Template.hello.helpers({
 		colorOptions: function () {
@@ -112,20 +118,25 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-	Meteor.startup(function () {
-		if (Colors.find().count() === 0){
-			var color = {name:"red", value: 1};
-			Colors.insert(color);
-		}
+	Meteor.methods({
+		reset : function(){
+			Colors.remove({});
+			Books.remove({});
+			if (Colors.find().count() === 0){
+				var color = {name:"red", value: 1};
+				Colors.insert(color);
+			}
 
-		if (Books.find().count() === 0){
-			var book1 = {title:"On The Road", author: "Jack Kerouac"};
-			var book2 = {title:"Basketball Diaries", author: "Jim Carroll"};
-			var book3 = {title:"Howl", author: "Allen Ginsberg"};
+			if (Books.find().count() === 0){
+				var book1 = {title:"On The Road", author: "Jack Kerouac"};
+				var book2 = {title:"Basketball Diaries", author: "Jim Carroll"};
+				var book3 = {title:"Howl", author: "Allen Ginsberg"};
 
-			Books.insert(book1);
-			Books.insert(book2);
-			Books.insert(book3);
+				Books.insert(book1);
+				Books.insert(book2);
+				Books.insert(book3);
+			}
+			return true;
 		}
-	});
+	})
 }
